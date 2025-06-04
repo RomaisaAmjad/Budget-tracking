2696cc624cf4c5bb7f33be3971c710cab5b6a565
Romaisa <iromaisa.22@gmail.com>
2025-06-04 16:03:15 +0500

    Changed the error page and applied a typing effect

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,12 +1,15 @@
 import ErrorImg from '../assets/404Error.png';
-import WorriedKid from '../assets/worriedKid.png';
+import WorriedKid from '../assets/WorriedKid.png';
+import TypingEffect from '../Components/TypingEffect.jsx';
 
 export default function PageNotFound() {
   return (
     <div className="  flex items-center min-h-screen bg-red-500 ">
-        <div  className='flex flex-row justify-around items-center'
-        > <img className='w-2/4 animate-bounce hover:cursor-pointer' src={ErrorImg} alt="Error 404" />
-        <img  className='w-2/4 hover:animate-pulse hover:cursor-pointer' src={WorriedKid} alt="Worried Kid" /></div>
+        <div  className='flex flex-col justify-around items-center'
+        > <img className='w-3/4 hover:cursor-pointer hover:animate-bounce' src={ErrorImg} alt="Error 404" />
+        {/* <img  className='w-2/4 hover:cursor-pointer' src={WorriedKid} alt="Worried Kid" /> */}
+        </div>
+        <TypingEffect/>
      
     </div>
   );
